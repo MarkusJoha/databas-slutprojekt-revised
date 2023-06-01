87dd84ca14f1f909ed36800b9034d2fc416703dc
MarkusJoha <112433408+MarkusJoha@users.noreply.github.com>
2023-06-01 18:32:28 +0200

    Delete databas-slutprojekt directory

--- databas-slutprojekt/src/Main.java
+++
@@ -1,9 +0,0 @@
-import DatabaseConnection.TableCreator;
-import views.MenuView;
-
-public class Main {
-    public static void main(String[] args) {
-        TableCreator.createTables();
-        MenuView.displayLoginMenu();
-    }
-}
